29df87ff17f46b3d89d8bf36854d07a3da30ab59
Omar Fernandez <omarferndz@gmail.com>
2019-01-22 13:52:26 -0400

    inicial con configuracion y rutas 08

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/name/{name}/lastname/{lastname?}', function ($name, $lastname = 'apellido'){
+    return 'Hola soy '. $name . $lastname;
+});
+
+Route::get('/mi_primera_ruta', function() {
+    return 'Hello World, esta es mi primera ruta :D.';
+});
