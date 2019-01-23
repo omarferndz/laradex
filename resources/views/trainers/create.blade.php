b5474a1086cada00cbd4309907002a23f51fbd1e
Omar Fernandez <omarferndz@gmail.com>
2019-01-23 13:23:21 -0400

    11view  creacion de la vista create a traves del controlador

---
+++ resources/views/trainers/create.blade.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="">
+
+</head>
+    <body>
+        <div class="container"><!--div container para que no tome toda la pg-->
+            <div class="form-group">
+                <label for="">Nombre</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
+
+        
+        
+        
+    </body>
+</html>
