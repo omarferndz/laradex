981a8d3234c0950b6ae1e03b1d89c45613ec8c74
Omar Fernandez <omarferndz@gmail.com>
2019-01-29 23:06:09 -0400

    19vista trainers, fronted a nuestravista index responsable de mostrar los trainers

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -6,11 +6,11 @@
 <div class="row">
     @foreach($trainers as $trainer)          
         <div class="col-sm">
-            <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="images/{{$trainer->avatar}}" alt="" class="src">
+            <div class="card text-center" style="width: 18rem; margin-top: 70px;">
+                <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;" class="card-img-top rounded-circle mx-auto d-block" src="images/{{$trainer->avatar}}" alt="" class="src">
                 <div class="card-body">
                     <h5 class="card-title">{{$trainer->name}}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">{{$trainer->description}}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
@@ -18,3 +18,4 @@
     @endforeach 
 </div>  
 @endsection
+
